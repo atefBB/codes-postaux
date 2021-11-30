2404efc8464d83a2ee4a2732b94f3412e74a21bd
ImenEdhri <imen.edhri@aureskonnect.com>
2021-11-30 08:50:27 +0100

    feat: get data from `full zip codes` file in `index` file

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-const index = {}
+const index = {};
 
-require('./zipCodes.json').forEach(entry => {
-  if (!(entry.zipCode in index)) {
-    index[entry.zipCode] = []
-  }
+require("./fullZipCodes.json")
+  .map((entry) => ({
+    zipCode: entry.codePostal,
+    townCode: entry.codeCommune,
+    townName: entry.nomCommune,
+    routingLabel: entry.libelleAcheminement,
+  }))
+  .forEach((entry) => {
+    if (!(entry.zipCode in index)) {
+      index[entry.zipCode] = [];
+    }
 
-  index[entry.zipCode].push(entry)
-})
+    index[entry.zipCode].push(entry);
+  });
 
 exports.find = function (postalCode) {
-  return index[postalCode] || []
-}
+  return index[postalCode] || [];
+};
